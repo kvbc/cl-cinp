f2cf0923bd65525cdf47b565613db0f31b986df6
kvbc <64214044+kvbc@users.noreply.github.com>
2020-09-28 18:31:38 +0200

    Update preprocessor

--- src/prepr.c
+++ src/prepr.c
@@ -12,20 +12,10 @@
 #include <string.h>
 
 
-cl_prepr_t* cl_prepr_new(void) {
+cl_prepr_t* cl_prepr_new(char* src) {
 	cl_prepr_t* prepr = malloc(sizeof(cl_prepr_t));
-	prepr->data = NULL;
-	prepr->lines = 0;
-	prepr->v_lines = NULL;
-	return prepr;
-}
-
-
-void cl_prepr_run(cl_prepr_t* prepr, char* src) {
-	if(prepr->data)	   cl_str_free(prepr->data);
-	if(prepr->v_lines) cl_vector_free(prepr->v_lines);
-
 	size_t len = strlen(src);
+
 	prepr->data = cl_str_substr(cl_str_new(), src, len);
 	prepr->lines = 1;
 	prepr->v_lines = cl_vector_new(50);
@@ -37,11 +27,13 @@ void cl_prepr_run(cl_prepr_t* prepr, char* src) {
 			++prepr->lines;
 			cl_vector_push(prepr->v_lines, i);
 		}
+
+	return prepr;
 }
 
 
 void cl_prepr_close(cl_prepr_t* prepr) {
-	if(prepr->data)	   cl_str_free(prepr->data);
-	if(prepr->v_lines) cl_vector_free(prepr->v_lines);
+	cl_str_free(prepr->data);
+	cl_vector_free(prepr->v_lines);
 	free(prepr);
 }
