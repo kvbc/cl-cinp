f2303b3ddb05c9fc3779c4e3cc4e53bdf6d78936
kvbc <64214044+kvbc@users.noreply.github.com>
2020-09-28 18:21:52 +0200

    Update str.h

--- src/str.h
+++ src/str.h
@@ -17,5 +17,5 @@ typedef struct {
 
 
 cl_str_t* cl_str_new(void);
-void cl_str_substr(cl_str_t* buff, char* src, size_t len);
+cl_str_t* cl_str_substr(cl_str_t* buff, char* src, size_t len);
 void cl_str_free(cl_str_t* str);
