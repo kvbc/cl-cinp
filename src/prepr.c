7321c116ce91c6e2a9382f5ccc8540e5a943cd20
kvbc <64214044+kvbc@users.noreply.github.com>
2020-09-28 18:20:22 +0200

    Updated prepr

--- src/prepr.c
+++ src/prepr.c
@@ -12,22 +12,25 @@
 #include <string.h>
 
 
-cl_prepr_t* cl_prepr_new(char* data) {
+cl_prepr_t* cl_prepr_new(void) {
 	cl_prepr_t* prepr = malloc(sizeof(cl_prepr_t));
-	prepr->data = data;
-	prepr->lines = 1;
-	prepr->v_lines = cl_vector_new(50);
+	prepr->data = NULL;
+	prepr->lines = 0;
+	prepr->v_lines = NULL;
 	return prepr;
 }
 
 
-void cl_prepr_run(cl_prepr_t* prepr) {
+void cl_prepr_run(cl_prepr_t* prepr, char* src) {
+	size_t len = strlen(src);
+	prepr->data = cl_str_substr(cl_str_new(), src, len);
+	prepr->lines = 1;
+	prepr->v_lines = cl_vector_new(50);
 	cl_vector_push(prepr->v_lines, 1);
-	size_t len = strlen(prepr->data);
 
 	for(size_t i = 0; i < len; ++i)
-		if(prepr->data[i] == '\n' ||
-		  (prepr->data[i] == '\r' && prepr->data[i + 1] == '\n')) {
+		if(src[i] == '\n' ||
+		  (src[i] == '\r' && src[i + 1] == '\n')) {
 			++prepr->lines;
 			cl_vector_push(prepr->v_lines, i);
 		}
@@ -35,6 +38,7 @@ void cl_prepr_run(cl_prepr_t* prepr) {
 
 
 void cl_prepr_close(cl_prepr_t* prepr) {
-	cl_vector_free(prepr->v_lines);
+	if(prepr->data)	   cl_str_free(prepr->data);
+	if(prepr->v_lines) cl_vector_free(prepr->v_lines);
 	free(prepr);
 }
