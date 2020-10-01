7f19b04d473aff0be36f691bd376b272a587c295
kvbc <64214044+kvbc@users.noreply.github.com>
2020-10-01 19:01:48 +0200

    Update lex.c

--- src/lex.c
+++ src/lex.c
@@ -66,7 +66,9 @@ static void cl_lex_absorbcmt(cl_lex_state_t* ls) {
 static void cl_lex_absorbws(cl_lex_state_t* ls) {
 	do ls->cur = cl_reader_readbyte(ls->rdr);
 	while(cl_char_isws(ls->cur));
-	cl_reader_skip(ls->rdr, -1);
+	if(ls->rdr->cur < ls->rdr->end)
+		// In case of EOF being the next token, there would be an infinite loop
+		cl_reader_skip(ls->rdr, -1);
 }
 
 
